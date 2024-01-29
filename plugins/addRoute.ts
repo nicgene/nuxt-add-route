430c0c4f927e6001a375e4aa93b9dd016ee7dad2
nicgene <dupkey@protonmail.com>
2024-01-28 22:44:54 -0600

    fix: import

--- plugins/addRoute.ts
+++ plugins/addRoute.ts
@@ -3,6 +3,7 @@ export default defineNuxtPlugin(async (nuxtApp) => {
   router.addRoute({
     name: "test",
     path: "/test",
-    component: () => import("~/components/test.vue"),
+    // https://github.com/nuxt/nuxt/issues/25428
+    component: () => import("~/components/test.vue").then(r => r.default || r),
   });
 });
